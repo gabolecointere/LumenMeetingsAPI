b5475b638cc450310d9e87b9a8582dbeb90718be
Gabriel lecointere <gabolecointere21@gmail.com>
2017-05-13 01:11:17 -0400

    Meetings app - RESTful Web Service with LUMEN

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$app->get('/', function () use ($app) {
+    return $app->version();
+});
+
+$app->group(['prefix' => 'api/v1'], function() use ($app) {
+  //Meeting related routes
+  $app->GET('meeting', [
+    'uses' => 'MeetingController@index'
+  ]);
+  $app->POST('meeting', [
+    'uses' => 'MeetingController@store'
+  ]);
+  $app->GET('meeting/{id}', [
+    'uses' => 'MeetingController@show'
+  ]);
+  $app->PATCH('meeting/{id}', [
+    'uses' => 'MeetingController@update'
+  ]);
+  $app->DELETE('meeting/{id}', [
+    'uses' => 'MeetingController@destroy'
+  ]);
+  //Registration in meeting related routes
+  $app->POST('meeting/registration', [
+    'uses' => 'RegistrationController@store'
+  ]);
+  $app->DELETE('meeting/registration/{id}', [
+    'uses' =>'RegistrationController@destroy'
+  ]);
+  //User authentication related routes
+  $app->POST('user', [
+    'uses' => 'AuthController@store'
+  ]);
+  $app->POST('user/signin', [
+    'uses' => 'AuthController@signin'
+  ]);
+});
